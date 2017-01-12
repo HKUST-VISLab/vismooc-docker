5d2434c80ab2b90aad3c2d3c5d9dba14e71195ce
zhutian <zhutian.chen@outlook.com>
2017-01-12 20:31:59 +0800

    use the image of data-server

--- data-server/dist/mathematician/Processor/__init__.py
+++
@@ -1,7 +0,0 @@
-'''Data processor
-'''
-
-from .new_hkvmooc import ParseLogFile, ParseUserFile, ParseCourseStructFile,\
-     ParseEnrollmentFile, DumpToDB, ExtractRawData
-__All__ = ["ParseLogFile", "ParseUserFile", "ParseCourseStructFile",
-           "ParseEnrollmentFile", "OutputFile", "DumpToDB", "ExtractRawData"]
--- data-server/dist/mathematician/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import DB, Processor
-from . import pipe, http_helper, fetch_data, logger
-
-__all__ = ["pipe", "http_helper", "fetch_data", "logger", "DB", "Processor"]
